fb975d2457517701900c9ed28905b68c9f4d982a
Etienne Chaput <etiennechaput@hotmail.com>
2025-04-11 12:17:51 -0400

    hg

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import TOS from '../pages/TermsOfService.vue'
 import PrivacyPolicy from '../pages/PrivacyPolicy.vue'
