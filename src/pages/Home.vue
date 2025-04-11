ee71fde1e5ce5cad7cdabc93a7340204f144fda3
Etienne Chaput <etiennechaput@hotmail.com>
2025-04-11 15:52:08 -0400

    test deploy main

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
       <v-container>
         <v-card class="pa-5" color="deep-purple-lighten-4">
           <v-card-title>Hello from Vuetify!</v-card-title>
-          <v-card-text>This is a Vuetify card inside a Vue page.</v-card-text>
+          <v-card-text>This is a Vuetify card inside a Vue page...</v-card-text>
           <v-btn color="primary">Click me</v-btn>
         </v-card>
       </v-container>
