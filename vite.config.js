1a1c07111834559e34232899f1872b62b494c51a
Etienne Chaput <etiennechaput@hotmail.com>
2025-04-11 10:12:19 -0400

    fd

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/bodybud-web/',
+  base: '/',
   resolve: {
     alias: {
       crypto: 'crypto-browserify',  
